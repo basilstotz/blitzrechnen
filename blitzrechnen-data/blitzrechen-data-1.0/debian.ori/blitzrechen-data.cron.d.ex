90645ee1ba374dbd698de3105c3ac7ee213b04d7
basilstotz <basil.stotz@gmail.com>
2014-09-25 12:25:33 +0200

    *

--- blitzrechnen-data/blitzrechen-data-1.0/debian.ori/blitzrechen-data.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the blitzrechen-data package
-#
-0 4	* * *	root	[ -x /usr/bin/blitzrechen-data_maintenance ] && /usr/bin/blitzrechen-data_maintenance
--- blitzrechnen-data/blitzrechen-data-1.0/debian.ori/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(blitzrechen-data):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="blitzrechen-data" command="/usr/bin/blitzrechen-data"
