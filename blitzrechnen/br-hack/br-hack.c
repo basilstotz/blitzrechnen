a3557fb7d488e0275533875094ef0b826e52757c
basilstotz <basil.stotz@gmail.com>
2015-07-08 10:50:29 +0200

    new br-hack

--- blitzrechnen/br-hack/br-hack.c
+++ blitzrechnen/br-hack/br-hack.c
@@ -2,6 +2,8 @@
 
 main()
 {
-  system("if cat mount|grep -v -q /usr/share/prop/blitzrechnen/nutzerdaten;then mount --bind /var/lib/blitzrechnen/nutzerdaten /usr/share/prop/blitzrechnen/nutzerdaten;fi");
-  system("chmod -R a+rw /usr/share/prop/blitzrechnen/nutzerdaten/");
+  setuid(0);
+  system("if test -d /usr/share/prop/blitzrechnen/Nutzerdaten;then rm -r /usr/share/prop/blitzrechnen/Nutzerdaten;fi");
+  system("cp -r /usr/share/prop/blitzrechnen/Nutzerdaten.default /usr/share/prop/blitzrechnen/Nutzerdaten");
+  system("chmod -R a+rw /usr/share/prop/blitzrechnen/Nutzerdaten/");
 }
